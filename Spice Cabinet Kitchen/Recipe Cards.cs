8b3be81224bddd4eb87308d1cd6db2966942f628
NightFox <codingminecrafter@gmail.com>
2023-09-14 15:08:53 -0400

    General code changes along with new code toolkit.

--- Spice Cabinet Kitchen/Recipe Cards.cs
+++ Spice Cabinet Kitchen/Recipe Cards.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
+using Toolkit;
 
 namespace Commands
 {
@@ -10,8 +9,9 @@ namespace Commands
     {
         public void Recipe()
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("You found a recipe box on the counter\nDo you want to open it?\n 1. Yes\n 2. No");
-            string OpenBox = (Console.ReadLine());
+            string? OpenBox = (Console.ReadLine());
 
             //opens box or leaves the program
             switch (OpenBox)
@@ -19,7 +19,7 @@ namespace Commands
                 case "1":
                     Console.WriteLine("----------------------------------");
                     Console.WriteLine("You open the box and find some recipe cards \nDo you want to read them? \n 1. Yes\n 2. No");
-                    string ReadCard = (Console.ReadLine());
+                    string? ReadCard = (Console.ReadLine());
                     switch (ReadCard)
                     {
                         case "1":
@@ -27,96 +27,49 @@ namespace Commands
                             Console.WriteLine("You pull out the recipe cards \nWhich recipe do you want to read?");
                             // Places for the recipe cards
                             Console.WriteLine("\n 1. Vanilla Cake\n 2. Pancakes \n insert other recipies here");
-                            string CardNumber = Console.ReadLine();
+
+                            string? CardNumber = Console.ReadLine();
                             switch (CardNumber)
                             {
                                 case "1":
                                     Console.Clear();
-                                    Console.WriteLine("----------------------------------");
-                                    Console.WriteLine("Vanilla Cake Recipe Card");
-                                    Console.OutputEncoding = System.Text.Encoding.UTF8;
-                                    Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\nIngredients:\r\n½ cup of butter\r\n1 ⅓ cups of sugar\r\n2 cups of all-purpose flour\r\n1 tbsp of baking powder\r\n½ tsp of salt\r\n1 cup of milk\r\n3 tsp of Vanilla extract\r\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\n1. Preheat the oven to 350°F\r\n2. Cream the butter and sugar together\r\n3. Add in eggs and stir\r\n4. Stir in milk and vanilla into the batter\r\n5. Combine the dry ingredients and stir into\r\nthe batter\r\n6. Bake until golden and allow to cool\r\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\nBake for 40 minutes at 350°F in a 9x13 pan\r\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\n");
-                                    Console.WriteLine("Press \"Z\" to continue");
-                                    Console.ReadKey();
-                                    Program.ReloopMain();
+                                    CommonTools.Continue("Vanilla Cake Recipe Card\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\nIngredients:\r\n½ cup of butter\r\n1 ⅓ cups of sugar\r\n2 cups of all-purpose flour\r\n1 tbsp of baking powder\r\n½ tsp of salt\r\n1 cup of milk\r\n3 tsp of Vanilla extract\r\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\n1. Preheat the oven to 350°F\r\n2. Cream the butter and sugar together\r\n3. Add in eggs and stir\r\n4. Stir in milk and vanilla into the batter\r\n5. Combine the dry ingredients and stir into\r\nthe batter\r\n6. Bake until golden and allow to cool\r\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\nBake for 40 minutes at 350°F in a 9x13 pan\r\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\n");
                                     break;
                                 case "2":
                                     Console.Clear();
-                                    Console.WriteLine("----------------------------------");
-                                    Console.WriteLine("Pancake Recipe Card");
-                                    Console.OutputEncoding = System.Text.Encoding.UTF8;
-                                    Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\nIngredients:\r\n1 ½ cups of all-purpose flour\r\n1 tbsp of baking powder\r\n1 tbsp of sugar\r\n1 pinch of salt\r\n1 ¼ cups of milk\r\n3 tbsp of butter\r\n1 egg\r\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\n1. Combine the flour, baking powder, sugar,\r\nand salt in a large bowl\r\n2. Melt the butter\r\n3. Pour the milk, egg, and butter into the dry\r\nIngredients\r\n4. Mix until generally smooth \r\n(a few lumps are fine)\r\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\nPour small amounts into the pan and flip \r\nto cook both sides\r\n");
-                                    Console.WriteLine("Press \"Z\" to continue");
-                                    Console.ReadKey();
-                                    Program.ReloopMain();
+                                    CommonTools.Continue("Pancake Recipe Card\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\nIngredients:\r\n1 ½ cups of all-purpose flour\r\n1 tbsp of baking powder\r\n1 tbsp of sugar\r\n1 pinch of salt\r\n1 ¼ cups of milk\r\n3 tbsp of butter\r\n1 egg\r\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\n1. Combine the flour, baking powder, sugar,\r\nand salt in a large bowl\r\n2. Melt the butter\r\n3. Pour the milk, egg, and butter into the dry\r\nIngredients\r\n4. Mix until generally smooth \r\n(a few lumps are fine)\r\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\nPour small amounts into the pan and flip \r\nto cook both sides\r\n");
                                     break;
                                 case "3":
                                     Console.Clear();
-                                    Console.WriteLine("----------------------------------");
-                                    Console.WriteLine("InsertFood Recipe Card");
-                                    Console.OutputEncoding = System.Text.Encoding.UTF8;
-                                    Console.WriteLine("Recipe goes here");
-                                    Console.WriteLine("Press \"Z\" to continue");
-                                    Console.ReadKey();
-                                    Program.ReloopMain();
+                                    CommonTools.Continue("Insert Food Recipe Card\nRecipe goes here");
                                     break;
                                 case "4":
                                     Console.Clear();
-                                    Console.WriteLine("----------------------------------");
-                                    Console.WriteLine("InsertFood Recipe Card");
-                                    Console.OutputEncoding = System.Text.Encoding.UTF8;
-                                    Console.WriteLine("Recipe goes here");
-                                    Console.WriteLine("Press \"Z\" to continue");
-                                    Console.ReadKey();
-                                    Program.ReloopMain();
+                                    CommonTools.Continue("Insert Food Recipe Card\nRecipe goes here");
                                     break;
 
                                 //returns to the main menu when invalid chars are inputed
                                 default:
-                                    Console.WriteLine("----------------------------------");
-                                    Console.WriteLine("That was not an option");
-                                    Console.WriteLine("Press \"Z\" to continue");
-                                    Console.ReadKey();
-                                    Program.ReloopMain();
+                                    CommonTools.Continue("That was not an option");
                                     break;
-
-                                    
                             }
                             break;
 
                         case "2":
-
-
-                        Console.WriteLine("----------------------------------");
-                            Console.WriteLine("You decide to examine the lovely box instead\nYou walk back to the kitchen island");
-                            Console.WriteLine("Press \"Z\" to continue");
-                            Console.ReadKey();
-                            Program.ReloopMain();
+                            CommonTools.Continue("You decide to examine the lovely box instead\nYou walk back to the kitchen island");
                             break;
-                        default: Console.WriteLine("That was not an option");
-                            Console.WriteLine("Press \"Z\" to continue");
-                            Console.ReadKey();
-                            Program.ReloopMain();
+                        default:
+                            CommonTools.Continue("That was not an option");
                             break;
                     }
                     break;
                 case "2":
-                    Console.WriteLine("----------------------------------");
-                    Console.WriteLine("You decide to not open the box\nYou walk back to the kitchen island");
-                    Console.WriteLine("Press \"Z\" to continue");
-                    Console.ReadKey();
-                    Program.ReloopMain();
+                    CommonTools.Continue("You decide to not open the box\nYou walk back to the kitchen island\n");
                     break;
                 default:
-                    Console.WriteLine("----------------------------------");
-                    Console.WriteLine("That was not an option");
-                    Console.WriteLine("Press \"Z\" to continue");
-                    Console.ReadKey();
-                    Program.ReloopMain();
+                    CommonTools.Continue("That was not an option");
                     break;
             }
-            
-                
         }
     }
 }
--- Spice Cabinet Kitchen/Testing Search Functions.cs
+++ Spice Cabinet Kitchen/Testing Search Functions.cs
@@ -13,9 +13,9 @@ namespace Commands
             //testing different methods of "searching" and displaying code ie searching cupcake, will bring up the 
             var food = new List<string>() { "Cake", "Cookies", "Pancake", "Cupcake", "Vanilla Pudding", "Fruit Snacks" };
 
-            string foodItem = Console.ReadLine();
+            string? foodItem = Console.ReadLine();
 
-            if (food.Contains(foodItem)) 
+            if (food.Contains(null)) 
             {
                 Console.WriteLine("Yes");
                 Console.ReadKey();
